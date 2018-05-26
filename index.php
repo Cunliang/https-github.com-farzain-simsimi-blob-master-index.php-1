bfe767b9281561f5a12b4442b82334242637b5fd
Cunliang <38970768+Cunliang@users.noreply.github.com>
2018-05-27 05:33:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,96 +1,41 @@
-<?php
-/*
-copyright @ medantechno.com
-Modified by Ilyasa
-And Modified by Farzain - zFz ( Faraaz )
-2017
-*/
-require_once('./line_class.php');
+const line = require('@line/bot-sdk');
+const express = require('express');
+const axios = require('axios');
 
-$channelAccessToken = 'WZUHIOUaT+PcdyATrNHHVe1eh6YiZF+XSWjRHxaiH9HGHilCPruWD2LfVNRP6gJx1p39aa1LFqWFgyl5hvesfnGYGL/Vo8qQJrRpfN/ndhJs1OPbnwA4xQ+qTRDS6/O9Dyf/4GE40d8SABmGoUsgXAdB04t89/1O/w1cDnyilFU='; //Your Channel Access Token
-$channelSecret = 'cef01e6774aa1778dacae1d481d39350';//Your Channel Secret
+const config = {
+  channelAccessToken: "jEFctXuQ+0y5wCcgyilX5hFx5TghXGe3zsLbEcQ1pm3GiRvPNPb/uSGvF1J7z8/LMaesMtrhwzfMkdYfTVVP06RX3Wl4QqfqebxrceVej3L45csspum/0kTtjPmsPbKZnBQF/vM14g0dmYBx60uukQdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "d3fd37632ca0ea18853d87ba11a72600",
+};
 
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+// create LINE SDK client
+const client = new line.Client(config);
+const app = express();
 
-$userId 	= $client->parseEvents()[0]['source']['userId'];
-$replyToken = $client->parseEvents()[0]['replyToken'];
-$message 	= $client->parseEvents()[0]['message'];
-$profil = $client->profil($userId);
-$pesan_datang = $message['text'];
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((e)=>{
+      console.log(e);
+    });
 
-if($message['type']=='sticker')
-{	
-	$balas = array(
-							'UserID' => $profil->userId,	
-                                                        'replyToken' => $replyToken,							
-							'messages' => array(
-								array(
-										'type' => 'text',									
-										'text' => 'Terima Kasih Stikernya.'										
-									
-									)
-							)
-						);
-						
+});
+
+function handleEvent(event) {
+  
+    if(event.message.text == "hai"){
+      const echo = { type: 'text', text: "Halo juga :)·" };
+      return client.replyMessage(event.replyToken, echo);
+    }
+
+    const echo = { type: 'text', text: "Saya tidak mengerti, saya simpan dulu" };
+    return client.replyMessage(event.replyToken, echo);
 }
-else
-$pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'df9b02ef-6b8a-4337-a383-80af09b0c6f0'; //API SimSimi
-$url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
-$json_data = file_get_contents($url);
-$url=json_decode($json_data,1);
-$diterima = $url['response'];
-if($message['type']=='text')
-{
-if($url['result'] == 404)
-	{
-		$balas = array(
-							'UserID' => $profil->userId,	
-                                                        'replyToken' => $replyToken,													
-							'messages' => array(
-								array(
-										'type' => 'text',					
-										'text' => 'Mohon Gunakan Bahasa Indonesia Yang Benar :D.'
-									)
-							)
-						);
-				
-	}
-else
-if($url['result'] != 100)
-	{
-		
-		
-		$balas = array(
-							'UserID' => $profil->userId,
-                                                        'replyToken' => $replyToken,														
-							'messages' => array(
-								array(
-										'type' => 'text',					
-										'text' => 'Maaf '.$profil->displayName.' Server Kami Sedang Sibuk Sekarang.'
-									)
-							)
-						);
-				
-	}
-	else{
-		$balas = array(
-							'UserID' => $profil->userId,
-                                                        'replyToken' => $replyToken,														
-							'messages' => array(
-								array(
-										'type' => 'text',					
-										'text' => ''.$diterima.''
-									)
-							)
-						);
-						
-	}
-}
- 
-$result =  json_encode($balas);
 
-file_put_contents('./reply.json',$result);
-
-
-$client->replyMessage($balas);
+// listen on port
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
